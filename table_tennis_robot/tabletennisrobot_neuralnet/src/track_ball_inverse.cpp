7ba2f028a3ef0ea31f60fd8a8edd4e72ba473440
dennis2110 <dennis2110@hotmail.com.tw>
2020-02-18 11:16:42 +0800

    update 2/18

--- table_tennis_robot/tabletennisrobot_neuralnet/src/track_ball_inverse.cpp
+++ table_tennis_robot/tabletennisrobot_neuralnet/src/track_ball_inverse.cpp
@@ -7,11 +7,18 @@
 
 #include "std_msgs/Float64MultiArray.h"
 
+bool is_ball_right = false, is_ball_left = false;
+bool start = true;
+bool changepos = false, changeside = false;
+//bool is_side_change = false;
+
+double slide_cmd = 0.0;
+double J1_cmd = 0.0;
 double ball_x = 0.0, ball_y = 0.0, ball_z = 0.0;
-double L1 = 0.26, L2 = 0.215, L3 = 0.26;
+double L1 = 0.253, L2 = 0.216, L3 = 0.26;
 double inv_y0, inv_z0;
 double inv_y1, inv_z1;
-double s_square;
+double s_square, s;
 double theta1 = 0.0, theta2 = 0.0, theta3 = 0.0, psi = 0.0, beta = 0.0, phi = 0.0;
 
 int main(int argc, char **argv)
@@ -35,7 +42,7 @@ int main(int argc, char **argv)
   // unified_cmd.data.push_back(3.6);
   // unified_cmd.data.push_back(0.26);
   // init pose (static)
-  unified_cmd.data.push_back(0.5); 
+  unified_cmd.data.push_back(0.4735); 
   unified_cmd.data.push_back(1.57);
   unified_cmd.data.push_back(2.2); //2.2 //1.1
   unified_cmd.data.push_back(2.9); //2.9 //4.2
@@ -55,29 +62,90 @@ int main(int argc, char **argv)
         ball_x = srv.response.pose.position.x;
         ball_y = srv.response.pose.position.y;
         ball_z = srv.response.pose.position.z;
+        if(start){
+          if(ball_y > 0.0){
+            is_ball_right = true;
+          }
+        }
         //ROS_INFO("ball pos x %f",srv.response.pose.position.x);
     }
-    inv_y1 = ball_y - L3;
+
+
+    if(is_ball_left)
+      inv_y1 = ball_y + L3;
+    else if(is_ball_right)
+      inv_y1 = ball_y - L3;
     inv_z1 = ball_z;
-    inv_y0 = 0.4735 - 0.5;
-    inv_z0 = 0.937;
+    inv_y0 = 0.4735 - unified_cmd.data.at(0);
+    inv_z0 = 0.94; //0.937
     s_square = pow(inv_y1-inv_y0, 2) + pow(inv_z1-inv_z0, 2);
+    s = sqrt(s_square);
 
-    theta2 = acos((s_square - pow(L1,2) - pow(L2, 2)) / (2*L1*L2));
-    beta = atan2(inv_z1-inv_z0, abs(inv_y1-inv_y0));
-    psi = acos((s_square + pow(L1,2) - pow(L2,2)) / (2*sqrt(s_square)*L1));
+    // is slide need change
+    if((s > (L1+L2)) || (s <= 0.25))
+      changepos = true;
+    else
+      changepos = false;
+    
+    //is J1 need change
+    if((ball_y >= 0.765 && is_ball_left) || (ball_y <= -0.765 && is_ball_right))
+      changeside = true;
+    else
+      changeside = false;
 
-    theta1 = beta + psi;
-    theta3 = phi - theta1 - theta2;
+    //reset hit ball side
+    if(changeside){
+      is_ball_right = false;
+      is_ball_left = false;
+    }
+    
+    //set hit ball, move slide
+    if((ball_y > -0.765 && ball_y < 0.765) && (changepos || changeside) || start){
+      if((ball_y > 0.0 && !is_ball_left) || is_ball_right){
+        is_ball_right = true;
+        slide_cmd = ball_y - 0.4;
+      }
+      if((ball_y < 0.0 && !is_ball_right) || is_ball_left){
+        is_ball_left = true;
+        slide_cmd = ball_y + 0.4;
+      }
+      unified_cmd.data.at(0) = slide_cmd;
+    }
 
-    ROS_INFO("value: %4.3f %4.3f %4.3f %4.3f %4.3f", theta2*57.3, beta*57.3, psi*57.3, theta1*57.3, theta3*57.3);
+    //change J1 side
+    if(changeside){
+      if(J1_cmd < 0.0){
+        J1_cmd = -1.57;
+      }else{
+        J1_cmd = 1.57;
+      }
+      unified_cmd.data.at(1) = J1_cmd;
+    }
+    ROS_INFO("##############################");
+    ROS_INFO("pos: %d, side: %d, ball_left: %d, ball_right: %d", changepos, changeside, is_ball_left, is_ball_right);
+    ROS_INFO("##############################");
+    if(s <= L1 + L2){
+      theta2 = -M_PIf64 + acos((s_square - pow(L1,2) - pow(L2, 2)) / (-2*L1*L2));
+      //theta2 = acos((-s_square + pow(L1,2) + pow(L2, 2)) / (2*L1*L2));
+      beta = atan2(inv_z1-inv_z0, inv_y1-inv_y0);
+      psi = acos((s_square + pow(L1,2) - pow(L2,2)) / (2*s*L1));
+      
+      theta1 = beta + psi;
+      theta3 = phi - theta1 - theta2;
+      ROS_INFO("s^2: %4.3f", s_square);
+      ROS_INFO("y0: %4.3f, z0: %4.3f, y1: %4.3f, z1: %4.3f", inv_y0, inv_z0, inv_y1, inv_z1);
+      ROS_INFO("value: %4.3f %4.3f %4.3f %4.3f %4.3f", theta2*57.3, beta*57.3, psi*57.3, theta1*57.3, theta3*57.3);
+      //ROS_INFO("value: %4.3f %4.3f %4.3f %4.3f %4.3f", theta2, beta, psi, theta1, theta3);
 
 
-    ROS_INFO("J2: %4.3f", 2.2-1.57-theta1);
-    unified_cmd.data.at(2) = 2.2-1.57-theta1;
-    //unified_cmd.data.at(3) = 2.9 + theta2;
-    //unified_cmd.data.at(4) = 2.2 - theta3;
+      //ROS_INFO("J3: %4.3f", 2.9 + theta2);
+      unified_cmd.data.at(2) = 0.63 + theta1;//2.2-1.57+theta1;
+      unified_cmd.data.at(3) = 2.9 - theta2;
+      unified_cmd.data.at(4) = 2.2 + theta3;
+      
+    }
     unified_cmd_pub.publish(unified_cmd);
+    start = false;
     ros::spinOnce();
     loop_rate.sleep();
   }
